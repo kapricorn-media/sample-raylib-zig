45fc19e102d0417069cf53aebedc407c15dc5af6
Jose Rico <jmrico01@gmail.com>
2024-06-24 17:18:30 -0600

    comment

--- build.zig
+++ build.zig
@@ -7,18 +7,21 @@ pub fn build(b: *std.Build) !void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib = try raylibBuild.addRaylib(b, target, optimize, .{
-        // Optional - build & link raygui.
-        .raygui = true,
-    });
-
     const exe = b.addExecutable(.{
         .name = "sample-raylib-zig",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    // Add raylib library and link with your executable. This is the only raylib boilerplate.
+    // Everything else in this file is standard Zig build system setup.
+    const raylib = try raylibBuild.addRaylib(b, target, optimize, .{
+        // Optional - build & link raygui.
+        .raygui = true,
+    });
     exe.linkLibrary(raylib);
+
     b.installArtifact(exe);
 
     const runCmd = b.addRunArtifact(exe);
